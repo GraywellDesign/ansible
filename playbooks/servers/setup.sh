997010707e67eb82595165cc89a4f08702f59439
Spencer Heywood <l.spencer.heywood@protonmail.com>
2022-06-11 23:41:59 -0600

    updated setup.sh to not run complete upgrade

--- playbooks/servers/setup.sh
+++ playbooks/servers/setup.sh
@@ -2,10 +2,11 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get update && apt-get install -y python3-pip vim git curl \
-  && echo 'Installed base packages' | tee -a /opt/setup.log
+apt-get update && apt-get install -y python3-pip vim git curl software-properties-common \
+    && echo 'Installed base packages' | tee -a /opt/setup.log
 
-pip3 install ansible
+apt-add-repository ppa:ansible/ansible -y && apt-get update && apt-get install -y ansible \
+    && echo 'Installed ansible' | tee -a /opt/setup.log
 
 ANSIBLE_LOG_PATH="/opt/ansible-setup.log" ansible-pull -U https://github.com/GraywellDesign/ansible setup.yml \
     && ANSIBLE_LOG_PATH="/opt/ansible-setup.log" ansible-pull -U https://github.com/GraywellDesign/ansible playbooks/servers/server.yml \
