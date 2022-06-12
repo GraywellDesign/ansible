04d59bc8001713a331ec598931ad68558a76e7db
Spencer Heywood <l.spencer.heywood@protonmail.com>
2022-06-12 11:58:19 -0600

    updated setup.sh

--- playbooks/servers/setup.sh
+++ playbooks/servers/setup.sh
@@ -5,7 +5,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update && apt-get install -y python3-pip vim git curl software-properties-common \
     && echo 'Installed base packages' | tee -a /opt/setup.log
 
-apt-add-repository ppa:ansible/ansible -y && apt-get update && apt-get install -y ansible \
+pip3 install ansible \
     && echo 'Installed ansible' | tee -a /opt/setup.log
 
 ANSIBLE_LOG_PATH="/opt/ansible-setup.log" ansible-pull -U https://github.com/GraywellDesign/ansible setup.yml \
